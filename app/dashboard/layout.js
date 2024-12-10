82b189b2e93b77e9ea21ba012a87d05fe89b5cfd
Muhammad Rizki Nur Ma'arif <muhammadrizkinur37@gmail.com>
2024-12-10 23:09:06 +0700

    add dashboard layout

--- app/dashboard/layout.js
+++ app/dashboard/layout.js
@@ -1,12 +1,70 @@
+import { AuthProvider } from "./auth-context";
+import { Calendar, Home, Inbox, Search, Settings } from "lucide-react"
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarInset,
+  SidebarProvider
+} from "@/components/ui/sidebar"
+import { cookies } from 'next/headers';
+import AppSidebarMenu from "@/components/AppSidebarMenu";
+import Header from "@/components/Header";
+
+const items = [
+  {
+    title: "Home",
+    url: "#",
+    icon: Home,
+  },
+  {
+    title: "Inbox",
+    url: "#",
+    icon: Inbox,
+  },
+  {
+    title: "Calendar",
+    url: "#",
+    icon: Calendar,
+  },
+  {
+    title: "Search",
+    url: "#",
+    icon: Search,
+  },
+  {
+    title: "Settings",
+    url: "#",
+    icon: Settings,
+  },
+]
+
 export const metadata = {
   title: 'Dashboard',
   description: 'Halaman dashboard',
 };
 
-export default async function DashboardLayout({ children }) {
+export default async function DashboardPageLayout({ children }) {
+  const cookieStore = await cookies();
+  const defaultOpen = cookieStore.get('sidebar:state')?.value === 'true';
   return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
+    <AuthProvider>
+      <html lang="en">
+        <body>
+          <SidebarProvider defaultOpen={defaultOpen}>
+            <AppSidebarMenu />
+            <SidebarInset>
+              <Header />
+              {children}
+            </SidebarInset>
+          </SidebarProvider>
+        </body>
+      </html>
+    </AuthProvider>
   )
 }
