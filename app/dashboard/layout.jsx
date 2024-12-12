5178f181c01dc31ffc17a1586144130988c99637
Muhammad Rizki Nur Ma'arif <muhammadrizkinur37@gmail.com>
2024-12-12 09:37:51 +0700

    add layout

---
+++ app/dashboard/layout.jsx
@@ -0,0 +1,32 @@
+import {
+  SidebarInset,
+  SidebarProvider
+} from "@/components/ui/sidebar"
+import { cookies } from 'next/headers';
+import AppSidebarMenu from "@/components/AppSidebarMenu";
+import Header from "@/components/Header";
+import { UserDataProvider } from "./user-data-context";
+
+export const metadata = {
+  title: 'Dashboard',
+  description: 'Halaman dashboard',
+};
+
+export default async function DashboardPageLayout({ children }) {
+  const cookieStore = await cookies();
+  const defaultOpen = cookieStore.get('sidebar:state')?.value === 'true';
+
+  return (
+    <UserDataProvider>
+      <SidebarProvider defaultOpen={defaultOpen}>
+        <AppSidebarMenu />
+        <SidebarInset>
+          <main>
+            <Header />
+            {children}
+          </main>
+        </SidebarInset>
+      </SidebarProvider>
+    </UserDataProvider>
+  )
+}
