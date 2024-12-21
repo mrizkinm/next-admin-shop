3cd1e99cebade0709f00bcb5c23454915bf2ee17
Muhammad Rizki Nur Ma'arif <muhammadrizkinur37@gmail.com>
2024-12-21 22:56:03 +0700

    add categories page with data fetching and UI components

--- app/dashboard/categories/page.jsx
+++ app/dashboard/categories/page.jsx
@@ -1,9 +1,54 @@
-import React from 'react'
+"use client"
 
-const page = () => {
+import Heading from '@/components/heading'
+import { Separator } from '@/components/ui/separator'
+import { buttonVariants } from '@/components/ui/button'
+import { cn } from '@/lib/utils'
+import { Plus } from 'lucide-react'
+import Link from 'next/link'
+import React, { useEffect, useState } from 'react'
+import { DataTable } from "@/components/ui/data-table";
+import { columns } from "./components/column";
+
+const CategoriesPage = () => {
+  const [ data, setData] = useState([])
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const response = await fetch("/api/data/categories", { method: "GET" });
+  
+        const responseData = await response.json();
+        setData(responseData);
+      } catch (error) {
+        console.error('Gagal mendapatkan akses token baru:', error.message);
+        throw error;
+      }
+    };
+    getData()
+  }, [])
+
   return (
-    <div>page</div>
+    <div className="h-full p-4 md:px-6">
+      <div className="space-y-4">
+        <div className="flex items-start justify-between">
+          <Heading
+            title="Categories"
+            description="List of category"
+          />
+          <Link
+            href="/dashboard/categories/add"
+            className={cn(buttonVariants())}
+            title="Add Category"
+          >
+            <Plus className="h-4 w-4" /> Add New
+          </Link>
+        </div>
+        <Separator />
+        <DataTable data={data} columns={columns} searchKey="name" />
+      </div>
+    </div>
   )
 }
 
-export default page
+export default CategoriesPage
