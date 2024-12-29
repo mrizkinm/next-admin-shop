b4d994ea5ad335100cfe294f7997497f81dd5953
Muhammad Rizki Nur Ma'arif <muhammadrizkinur37@gmail.com>
2024-12-29 20:00:21 +0700

    feat: implement GET and POST endpoints for order management

---
+++ app/api/data/orders/route.js
@@ -0,0 +1,53 @@
+import db from "@/lib/db";
+import { NextResponse } from "next/server"
+import { z } from "zod";
+
+export async function GET(req) {
+  try {
+    const order = await db.order.findMany({
+      include: {
+        items: true,
+        customer: true
+      },
+    })
+
+    return NextResponse.json(order)
+  } catch (error) {
+    console.error("Error get data", error)
+    return NextResponse.json({ error: "Internal server error" }, {status: 500})
+  }
+}
+
+const formSchema = z.object({
+  name: z.string().min(1)
+});
+
+export async function POST(req) {
+  try {
+    const { name } = await req.json();
+
+    const result = formSchema.safeParse({ name });
+    
+    if (!result.success) {
+      const errors = result.error.flatten().fieldErrors;
+
+      // Ubah format menjadi { fieldName: "Error message" }
+      const simplifiedErrors = Object.fromEntries(
+        Object.entries(errors).map(([key, value]) => [key, value?.[0] || 'Invalid value'])
+      );
+
+      return NextResponse.json({ errors: simplifiedErrors }, { status: 400 });
+    }
+
+    const order = await db.order.create({
+      data: {
+        name
+      }
+    })
+
+    return NextResponse.json(order)
+  } catch (error) {
+    console.log("ERROR order POST", error)
+    return NextResponse.json({ errors: "Internal Error" }, {status: 500})
+  }
+}
