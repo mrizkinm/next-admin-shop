41f5098bee60f31af20d81f5a714c29c97db9e8d
Muhammad Rizki Nur Ma'arif <muhammadrizkinur37@gmail.com>
2024-12-29 20:00:07 +0700

    feat: implement CRUD operations for customer data management

---
+++ app/api/data/customers/[customerId]/route.js
@@ -0,0 +1,89 @@
+import db from "@/lib/db";
+import { NextResponse } from "next/server";
+import { z } from "zod";
+
+export async function GET(req, {params}) {
+  try {
+    if (!params.customerId) {
+      return new NextResponse({ error: "Harus ada customer id" }, {status: 400})
+    }
+
+    const customer = await db.customer.findUnique({
+      where: {
+        id: parseInt(params.customerId)
+      },
+      include: {
+        orders: true
+      }
+    })
+    return NextResponse.json(customer);
+  } catch (error) {
+    console.log('ERROR customer GET', error);
+    return NextResponse({ error: "Internal server error" }, {status: 500})
+  }
+}
+
+const formSchema = z.object({
+  name: z.string().min(1),
+  email: z.string().email().min(1),
+  phone: z.string().min(1),
+  address: z.string().min(1)
+});
+
+export async function PATCH(req, {params}) {
+  try {
+    if (!params.customerId) {
+      return new NextResponse({ error: "Harus ada customer id" }, {status: 400})
+    }
+    
+    const { name, email, phone, address } = await req.json();
+
+    const result = formSchema.safeParse({ name, email, phone, address });
+
+    if (!result.success) {
+      const errors = result.error.flatten().fieldErrors;
+
+      // Ubah format menjadi { fieldName: "Error message" }
+      const simplifiedErrors = Object.fromEntries(
+        Object.entries(errors).map(([key, value]) => [key, value?.[0] || 'Invalid value'])
+      );
+
+      return NextResponse.json({ errors: simplifiedErrors }, { status: 400 });
+    }
+
+    await db.customer.updateMany({
+      where: {
+        id: parseInt(params.customerId)
+      },
+      data: {
+        name,
+        email,
+        phone,
+        address
+      }
+    })
+
+    return NextResponse.json({ msg: "Success to update data" });
+  } catch (error) {
+    console.error('Error patch data', error);
+    return new NextResponse({ error: "Internal server error" }, {status: 500})
+  }
+}
+
+export async function DELETE(req, {params}) {
+  try {
+    if (!params.customerId) {
+      return new NextResponse({ error: "Harus ada customer id" }, {status: 400})
+    }
+
+    await db.customer.deleteMany({
+      where: {
+        id: parseInt(params.customerId)
+      }
+    })
+    return NextResponse.json({ msg: "Success to delete data" });
+  } catch (error) {
+    console.log('Error delete data', error);
+    return new NextResponse({ error: "Internal server error" }, {status: 500})
+  }
+}
