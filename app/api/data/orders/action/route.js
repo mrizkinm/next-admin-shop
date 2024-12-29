fd38cc24ee182389579bac402f8b06c009f2c128
Muhammad Rizki Nur Ma'arif <muhammadrizkinur37@gmail.com>
2024-12-29 22:59:36 +0700

    feat: implement POST endpoint for order processing and cancellation with stock update

---
+++ app/api/data/orders/action/route.js
@@ -0,0 +1,93 @@
+import db from "@/lib/db";
+import { NextResponse } from "next/server"
+
+export async function POST(req) {
+  try {
+    const { action, id, items } = await req.json();
+    let status = "";
+    if (action === "process") {
+      await db.order.updateMany({
+        where: {
+          id: parseInt(id)
+        },
+        data: {
+          status: "Processed"
+        }
+      })
+      status = "Processed"
+    }
+    if (action === "cancel") {
+      const result = await updateProductStockFromOrder(id, items);
+      status = "Canceled"
+      if (!result.success) {
+        return NextResponse.json({ errors: result.error }, { status: result.httpStatus });
+      }
+    }
+
+    return NextResponse.json({msg: `Success to ${action} order ${id}`, status: status})
+  } catch (error) {
+    console.log("ERROR order POST", error)
+    return NextResponse.json({ errors: "Internal Error" }, {status: 500})
+  }
+}
+
+async function updateProductStockFromOrder(orderId, items) {
+  try {
+    const updateProductQuantities = await db.$transaction(async (tx) => {
+      // Update status order menjadi Canceled
+      await tx.order.updateMany({
+        where: {
+          id: parseInt(orderId)
+        },
+        data: {
+          status: "Canceled"
+        }
+      });
+ 
+      // Cek stok untuk semua produk
+      const products = await Promise.all(items.map(item =>
+        tx.product.findUnique({
+          where: { id: item.productId }
+        })
+      ));
+ 
+      // Validasi stok
+      items.forEach((item, index) => {
+        const product = products[index];
+        if (!product) {
+          return NextResponse.json({ errors: `Product with id ${item.productId} not found`}, { status: 400 });
+        }
+        if (product.quantity < item.quantity) {
+          return NextResponse.json({ errors: `Insufficient stock for product ${item.productId}`}, { status: 400 });
+        }
+      });
+ 
+      // Jika semua stok mencukupi, lakukan update
+      const updates = await Promise.all(items.map(item =>
+        tx.product.update({
+          where: { id: item.productId },
+          data: {
+            quantity: {
+              increment: item.quantity
+            }
+          }
+        })
+      ));
+ 
+      return updates;
+    });
+ 
+    return {
+      success: true,
+      data: updateProductQuantities,
+      httpStatus: 200
+    };
+ 
+  } catch (error) {
+    return {
+      success: false,
+      error: error.message,
+      httpStatus: 500
+    };
+  }
+ }
