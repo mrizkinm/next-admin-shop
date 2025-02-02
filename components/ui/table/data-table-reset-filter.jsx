468678a4c661ff2b5e169ab737ec9a4a96cb2997
Muhammad Rizki Nur Ma'arif <muhammadrizkinur37@gmail.com>
2025-02-02 20:53:44 +0700

    feat: add DataTableResetFilter component for resetting active filters in DataTable

---
+++ components/ui/table/data-table-reset-filter.jsx
@@ -0,0 +1,18 @@
+'use client';
+
+import { Button } from '../button';
+
+export function DataTableResetFilter({
+  isFilterActive,
+  onReset
+}) {
+  return (
+    <>
+      {isFilterActive ? (
+        <Button variant='outline' onClick={onReset}>
+          Reset Filters
+        </Button>
+      ) : null}
+    </>
+  );
+}
