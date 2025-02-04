353e9bd1e1d210da65558386a4915d13b0405ac0
Muhammad Rizki Nur Ma'arif <muhammadrizkinur37@gmail.com>
2025-02-04 09:53:43 +0700

    refactor: rename route.js to route.ts and add type annotation for req parameter

--- app/api/auth/logout/route.ts
+++ app/api/auth/logout/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import db from "@/lib/db";
 
-export async function POST(req) {
+export async function POST(req: Request) {
   const { id } = await req.json();
 
   if (!id) {
