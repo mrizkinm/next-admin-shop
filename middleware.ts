cdf89fbf43cfc643145b7d6f8e343edc590b3213
Muhammad Rizki Nur Ma'arif <muhammadrizkinur37@gmail.com>
2025-02-18 13:47:52 +0700

    update middleware

--- middleware.ts
+++ middleware.ts
@@ -1,37 +1,53 @@
-import { NextRequest, NextResponse } from 'next/server';
-import { jwtVerify } from 'jose';
+// import { NextRequest, NextResponse } from 'next/server';
+// import { jwtVerify } from 'jose';
+import { withAuth } from "next-auth/middleware";
 
-export async function middleware(req: NextRequest) {
-  const refreshToken = req.cookies.get('refreshToken');
+export default withAuth({
+  pages: {
+    signIn: "/login",
+  },
+});
 
-  // Redirect ke login jika tidak ada token
-  if (!refreshToken) {
-    return NextResponse.redirect(new URL('/login', req.url));
-  }
+// export async function middleware(req: NextRequest) {
+//   const refreshToken = req.cookies.get('refreshToken');
 
-  try {
-    const { payload }: { payload: { id: string } } = await jwtVerify(
-      refreshToken.value,
-      new TextEncoder().encode(process.env.REFRESH_TOKEN_SECRET)
-    );
+//   // Redirect ke login jika tidak ada token
+//   if (!refreshToken) {
+//     return NextResponse.redirect(new URL('/login', req.url));
+//   }
 
-    // Set user info (optional) to request headers
-    const requestHeaders = new Headers(req.headers);
-    requestHeaders.set('x-user-id', payload.id);
+//   try {
+//     const { payload }: any = await jwtVerify(
+//       refreshToken.value,
+//       new TextEncoder().encode(process.env.REFRESH_TOKEN_SECRET)
+//     );
 
-    // Lanjutkan ke halaman tujuan
-    return NextResponse.next({
-      request: {
-        headers: requestHeaders,
-      },
-    });
-  } catch (error: any) {
-    // Jika token kadaluwarsa atau tidak valid, arahkan ke halaman login
-    console.error('JWT verification failed:', error.message);
-    return NextResponse.redirect(new URL('/login', req.url));
-  }
-}
+//     // Set user info (optional) to request headers
+//     const requestHeaders = new Headers(req.headers);
+//     requestHeaders.set('x-user-id', payload.id);
+
+//     // Lanjutkan ke halaman tujuan
+//     return NextResponse.next({
+//       request: {
+//         headers: requestHeaders,
+//       },
+//     });
+//   } catch (error: any) {
+//     // Jika token kadaluwarsa atau tidak valid, arahkan ke halaman login
+//     console.error('JWT verification failed:', error.message);
+//     // return NextResponse.redirect(new URL('/login', req.url));
+//   }
+// }
 
 export const config = {
-  matcher: ['/dashboard/:path*', '/api/data/:path*'],
+  matcher: [
+    '/dashboard/:path*',
+    // '/api/category/:path*',
+    // '/api/customer/:path*',
+    // '/api/order/:path*',
+    // '/api/product/:path*',
+    // '/api/shop/:path*',
+    // '/api/user/password',
+    // '/api/user/profile'
+  ],
 };
