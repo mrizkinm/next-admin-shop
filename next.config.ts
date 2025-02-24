928afc137ca6249c239419ab3d6481124fe76664
Muhammad Rizki Nur Ma'arif <muhammadrizkinur37@gmail.com>
2025-02-24 10:07:29 +0700

    feat: configure remote image patterns for localhost in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,14 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {}
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+      }
+    ], // Tambahkan domain di sini
+  },
+};
 
 export default nextConfig;
