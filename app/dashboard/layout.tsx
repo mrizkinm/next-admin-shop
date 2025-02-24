390aca8731ac64e35985cac9d4f83e68d4695d1a
Muhammad Rizki Nur Ma'arif <muhammadrizkinur37@gmail.com>
2025-02-24 10:08:37 +0700

    feat: update DashboardPageLayout to dynamically generate metadata and replace UserDataProvider with StoreProvider

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -5,33 +5,45 @@ import {
 import { cookies } from 'next/headers';
 import AppSidebarMenu from "@/components/app-sidebar-menu";
 import Header from "@/components/header";
-import { UserDataProvider } from "../../context/user-data-context";
+// import { UserDataProvider } from "../../context/user-data-context";
 import { NuqsAdapter } from 'nuqs/adapters/next/app';
+import { getStoreInfo } from "@/lib/api";
+import { StoreProvider } from "@/context/store-context";
 
-export const metadata = {
-  title: 'Dashboard',
-  description: 'Halaman dashboard',
-};
+export async function generateMetadata() {
+  // Ambil data store secara dinamis
+  const store = await getStoreInfo();
+  const storeInfo = store;
+
+  return {
+    title: storeInfo.name,
+    description: storeInfo.description,
+  };
+}
 
 export default async function DashboardPageLayout({ children }: Readonly<{
   children: React.ReactNode;
 }>) {
   const cookieStore = await cookies();
   const defaultOpen = cookieStore.get('sidebar:state')?.value === 'true';
+  const store = await getStoreInfo();
+  const storeInfo = store;
 
   return (
-    <UserDataProvider>
-      <SidebarProvider defaultOpen={defaultOpen}>
-        <AppSidebarMenu />
-        <SidebarInset>
-          <NuqsAdapter>
-            <main>
-              <Header />
-              {children}
-            </main>
-          </NuqsAdapter>
-        </SidebarInset>
-      </SidebarProvider>
-    </UserDataProvider>
+    // <UserDataProvider>
+      <StoreProvider storeInfo={storeInfo}>
+        <SidebarProvider defaultOpen={defaultOpen}>
+          <AppSidebarMenu />
+          <SidebarInset>
+            <NuqsAdapter>
+              <main>
+                <Header />
+                {children}
+              </main>
+            </NuqsAdapter>
+          </SidebarInset>
+        </SidebarProvider>
+      </StoreProvider>
+    // </UserDataProvider>
   )
 }
